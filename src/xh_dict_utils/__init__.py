af2b1f839a08ff4fd6dd834d0fcf66e0336e3786
xeth hung <adamhung12@gmail.com>
2023-04-17 12:25:40 +0800

    fix directory pointing

--- src/xh_dict_utils/__init__.py
+++ src/xh_dict_utils/__init__.py
@@ -1,6 +1,6 @@
-from dict_utils import EntryType, Selector, Entry, Entries
+from .dict_utils import EntryType, Selector, Entry, Entries
 import ExtractFromFile
-from app import main
+from .app import main
 
 
 if __name__ == "__main__":
