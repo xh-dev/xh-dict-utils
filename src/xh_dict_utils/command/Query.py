d6bfc05c8e5a8d1350ada56908f0d43bdb032a5c
xethhung <adamhung12@gmail.com>
2023-04-19 13:17:27 +0800

    add installation dependencies and fix  is None bug

--- src/xh_dict_utils/command/Query.py
+++ src/xh_dict_utils/command/Query.py
@@ -11,6 +11,8 @@ class Query(CommandTemplate):
         super().__init__("query", "query by selector")
 
     def parserOp(self, parser: argparse.ArgumentParser):
+        parser.add_argument("--in-format", choice=["yaml", "json"], default="yaml")
+        parser.add_argument("--in-format-json", dest="in_format", action="store_const", const="json")
         parser.add_argument("--selector", type=str)
         parser.add_argument("--output", type=str, default="{selector} -> {value}")
         parser.add_argument("--exact", dest="notation_type", action="store_const", const="exact")
@@ -18,7 +20,7 @@ class Query(CommandTemplate):
         parser.set_defaults(notation_type="notation")
         parser.set_defaults(exact=False)
 
-    def handleOp(self, argv: [str], sys=None):
+    def handleOp(self, argv: [str]):
         selector = argv.selector
         if argv.notation_type == "exact":
             findings = Entries.from_dict(yaml.safe_load(sys.stdin)).match_exact(selector)
